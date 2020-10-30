cc7e9089dd8b041d516fa6436db4abcd6242dd27
maymax777 <dev.bugger777@yahoo.com>
2020-10-29 21:02:32 -0400

    Add initial SearchBox, VideoPlayer components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,19 @@
-import './style.css';
+import { Layout } from 'antd'
+import SearchBox from '../SearchBox';
+import VideoPlayer from '../VideoPlayer';
+import 'antd/dist/antd.css';
+import style from './style.scss'
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <title>Youtube Player</title>
-      </header>
+    <div >
+      <Layout className={style.container}>
+        <Content>
+          <SearchBox />
+          <VideoPlayer />
+        </Content>
+      </Layout>    
     </div>
   );
 }
