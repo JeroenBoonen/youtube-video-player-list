820e0e6219abccdf7d29b66590d351739c268599
maymax777 <dev.bugger777@yahoo.com>
2020-10-29 20:31:46 -0400

    Restruct Folder

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title>Youtube Player</title>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/css/style.css';
+import App from './components/App';
+import reportWebVitals from './services/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
